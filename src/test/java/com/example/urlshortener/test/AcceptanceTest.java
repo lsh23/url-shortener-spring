c406aee68d79dce586dddb1087ef2117832efdf7
lsh23 <shseoul14@gmail.com>
2023-06-07 18:48:19 +0900

    test : AcceptanceTest 베이스 class 추가

---
+++ src/test/java/com/example/urlshortener/test/AcceptanceTest.java
@@ -0,0 +1,19 @@
+package com.example.urlshortener.test;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AcceptanceTest {
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public void setUp() {
+        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+            RestAssured.port = port;
+        }
+    }
+}
