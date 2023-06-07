04c17a2357b843250a0a7e91a3981955bd78eedf
lsh23 <shseoul14@gmail.com>
2023-06-07 19:05:46 +0900

    test : integrationTest 베이스 class 추가

---
+++ src/test/java/com/example/urlshortener/test/IntegrationTest.java
@@ -0,0 +1,9 @@
+package com.example.urlshortener.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootTest
+@Profile("test")
+public class IntegrationTest {
+}
